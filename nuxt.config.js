1a618ddea3e8144ac473942b8cee678d7c82f7a5
Thomas Grinderslev <thomas@fdfnivaa.dk>
2019-01-29 18:43:07 +0100

    Updated title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,18 @@ const pkg = require('./package')
 module.exports = {
   mode: 'universal',
 
+  meta: {
+    name: 'FDF Nivå',
+    description: 'Kom og leg med!'
+  },
+
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
