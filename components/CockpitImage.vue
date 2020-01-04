31b2e658e8ea4ad41853b709eca281973847a28c
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-01-04 15:40:51 +0100

    Added component for displaying images from Cockpit

---
+++ components/CockpitImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    :src="fullSource"
+    :alt="alt"
+  >
+</template>
+
+<script>
+  import {cockpit as cockpitConfig} from '~/cockpit-config'
+  export default {
+    name: 'CockpitImage',
+    props: {
+      src: {
+        type: String,
+        required: true
+      },
+      alt: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      fullSource() {
+        return cockpitConfig.url + this.src
+      }
+    }
+  }
+</script>
