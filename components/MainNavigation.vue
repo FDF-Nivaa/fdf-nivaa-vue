81ddc98ab2e7a5def6e58c3878f7f5b851d1a24c
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-01-04 21:46:09 +0100

    Improved main navigation layout

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -56,38 +56,71 @@
   export default {
     components: { CompassIcon, RepeatIcon, UsersIcon }
   }
+</script>
+
+<style
+  lang="less"
+  scoped
+>
   .main-navigation {
-    position: fixed;
+    position: sticky;
     top: 0;
     left: 0;
     right: 0;
-    background: rgba(0, 0, 0, .5);
+    background: @duskBlue;
   }
 
   .main-navigation-list {
-    max-width: 50em;
-    list-style: none;
     display: flex;
     flex-direction: row;
+    max-width: 50em;
+    margin: 0 auto;
+    list-style: none;
+    align-items: stretch;
   }
 
   .main-navigation-list-item {
+    display: flex;
     flex: 1 auto 0;
+    margin: 0;
   }
 
   .main-navigation-link {
     vertical-align: middle;
-    display: block;
+    display: flex;
     color: white;
     padding: 1em 1.5em;
+    text-decoration: none;
+    margin: 0;
+    flex-direction: row;
+    align-items: center;
+
+    @media (hover: hover) {
+      &:hover {
+        background: @nightBlue;
+      }
+    }
   }
+
+  .main-navigation-link-icon {
+    display: block;
+    font-size: 2.5em;
+    width: 1em;
+    height: 1em;
+  }
+  .main-navigation-link-text {
+    display: block;
+    margin-left: 1em;
+  }
+
 
   .main-navigation-logo-link {
     padding: 0;
+    flex-grow: 1;
   }
 
   .main-navigation-logo {
     display: block;
-    height: 3em;
+    height: 4em;
   }
 </style>
