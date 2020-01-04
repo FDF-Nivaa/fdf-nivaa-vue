b617455c32b98eb64bfcae2b441d2630834eb81e
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-01-04 21:45:53 +0100

    Added site config file

---
+++ site.config.js
@@ -0,0 +1,8 @@
+export default {
+  facebookUrl: 'https://www.facebook.com/FDFNivaa/',
+  presentationalFacebookUrl: 'facebook.com/FDFNivaa',
+  hashtag: '#FDFNivå',
+  email: 'Hej@FDFNivaa.dk',
+  instagramUrl: 'https://www.instagram.com/fdfnivaa/',
+  instagramHashtagUrl: 'https://www.instagram.com/explore/tags/fdfnivaa/'
+}
