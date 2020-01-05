13ca9bc5f71922a0a0207a4b518dee4485976f06
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-01-05 20:54:51 +0100

    Added calendar label component

---
+++ components/CalendarLabel.vue
@@ -0,0 +1,54 @@
+<template>
+  <span
+    class="calendar-label"
+    :class="calendar.className"
+  >{{calendar.name}}</span>
+</template>
+
+<script>
+  export default {
+    name: 'CalendarLabel',
+    props: {
+      calendar: {
+        type: Object,
+        required: true,
+        validator(calendar) {
+          return calendar.className && calendar.name
+        }
+      }
+    }
+  }
+</script>
+
+<style
+  lang="less"
+  scoped
+>
+  .calendar-label {
+    background: @duskBlue;
+    color: white;
+    border-radius: .2rem;
+    padding: .25em .5em;
+    font-size: .75em;
+
+    &.tumlinge {
+      background: @tumlingeColor;
+    }
+
+    &.pilte {
+      background: @pilteColor;
+    }
+
+    &.vaebnere {
+      background: @vaebnereColor;
+    }
+
+    &.seniorvaebnere {
+      background: @seniorvaebnereColor;
+    }
+
+    &.seniorer {
+      background: @seniorerColor;
+    }
+  }
+</style>
