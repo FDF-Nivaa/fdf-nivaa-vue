4b420fc00dd445ec8e3463351da816c637cade72
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-01-26 21:25:02 +0100

    Added more date formatting options

--- utils/format.js
+++ utils/format.js
@@ -1,3 +1,8 @@
+import format from 'date-fns/format'
+import daLocale from 'date-fns/locale/da'
+
+const globalFormattingOptions = { locale: daLocale }
+
 /**
  * Formats the given date in a human readable way.
  * @param {Date} date
@@ -5,22 +10,108 @@
  * @return {string}
  */
 export function formatDate(date, includeTime) {
-  const formattedDate = date.toLocaleDateString(
-    'da',
-    {
-      dateStyle: 'long',
-    }
-  )
+  let outputFormat = 'do MMMM'
 
-  if (includeTime) {
-    return formattedDate + ' kl. ' + date.toLocaleDateString(
-      'da',
-      {
-        timeStyle: 'short'
-      }
-    )
+  if (!isCurrentYear(date)) {
+    outputFormat += ' y'
   }
 
-  return formattedDate
+  if (includeTime) {
+    outputFormat += ` 'kl.' k:mm`
+  }
 
+  return format(date, outputFormat, globalFormattingOptions)
 }
+
+/**
+ * Formats the given date span in a human readable way.
+ * @param {Date} date1
+ * @param {Date} date2
+ * @param {boolean=} includeTime
+ * @return {string}
+ */
+export function formatDateSpan(date1, date2, includeTime) {
+  if (isSameDay(date1, date2)) {
+    if (includeTime) {
+      return `${formatDate(date1)} kl. ${format(date1, 'k:mm', globalFormattingOptions)} - ${format(date2, 'k:mm', globalFormattingOptions)}`
+    }
+
+    return formatDate(date1)
+  }
+
+  let outputFormat1 = 'do'
+  let outputFormat2 = 'do MMMM'
+
+  if (includeTime) {
+    outputFormat1 = outputFormat2
+  }
+
+  if (isSameYear(date1, date2)) {
+    if (!isCurrentYear(date2)) {
+      outputFormat2 += ' y'
+    }
+  } else {
+    outputFormat1 += ' y'
+    outputFormat2 += ' y'
+  }
+
+  if (includeTime) {
+    outputFormat1 += ` 'kl.' k:mm`
+    outputFormat2 += ` 'kl.' k:mm`
+  }
+
+  const formattedDate1 = format(date1, outputFormat1, globalFormattingOptions)
+  const formattedDate2 = format(date2, outputFormat2, globalFormattingOptions)
+
+  return `${formattedDate1} - ${formattedDate2}`
+}
+
+/**
+ * Returns true if the given date is in the current year
+ * @param {Date} date
+ * @return {boolean}
+ */
+export function isCurrentYear(date) {
+  return date.getFullYear() === new Date().getFullYear()
+}
+
+/**
+ * Returns true if the given dates are in the same year
+ * @param {Date} date1
+ * @param {Date} date2
+ * @return {boolean}
+ */
+export function isSameYear(date1, date2) {
+  return date1.getFullYear() === date2.getFullYear()
+}
+
+/**
+ * Returns true if the given dates are in the same month
+ * @param {Date} date1
+ * @param {Date} date2
+ * @return {boolean}
+ */
+export function isSameMonth(date1, date2) {
+  return date1.getMonth() === date2.getMonth()
+}
+
+/**
+ * Returns true if the given dates are on the same date
+ * @param {Date} date1
+ * @param {Date} date2
+ * @return {boolean}
+ */
+export function isSameDate(date1, date2) {
+  return date1.getDate() === date2.getDate()
+}
+
+/**
+ * Returns true if the given dates are in the same day
+ * @param {Date} date1
+ * @param {Date} date2
+ * @return {boolean}
+ */
+export function isSameDay(date1, date2) {
+  return isSameYear(date1, date2) && isSameMonth(date1, date2) && isSameDate(date1, date2)
+}
+
