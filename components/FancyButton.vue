cdbcf9a268e361f9d3dd8fa36c3b937a09b347de
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-02-02 21:29:31 +0100

    Fixed a bug in the nuxt link support in the fancy button and added ghost style

--- components/FancyButton.vue
+++ components/FancyButton.vue
@@ -1,27 +1,45 @@
 <template>
-  <component class="fancy-button" :is="tagName" :type="type" :to="linkTo">
-    <slot/>
+  <component
+    class="fancy-button"
+    :class="variant"
+    :is="tagName"
+    :type="type"
+    :to="to"
+  >
+    <slot />
   </component>
 </template>
 
 <script>
+  export const Variants = {
+    FLAT: 'flat',
+    GHOST: 'ghost'
+  }
   export default {
     props: {
       href: {
         type: String,
         required: false
       },
-      linkTo: {
+      to: {
         type: String,
         required: false
+      },
+      variant: {
+        type: String,
+        required: false,
+        default: Variants.GHOST,
+        validator(variant) {
+          return Object.values(Variants).includes(variant)
+        }
       }
     },
     computed: {
       isLink() {
-        return !!(this.href || this.linkTo)
+        return !!(this.href || this.to)
       },
       tagName() {
-        if (this.linkTo) {
+        if (this.to) {
           return 'nuxt-link'
         }
         if (this.href) {
@@ -37,20 +55,45 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style
+  lang="scss"
+  scoped
+>
   .fancy-button {
     background: $duskBlue;
+    border: 1px solid $duskBlue;
     border-radius: $defaultBorderRadius;
     padding: .5em .75em;
     text-decoration: none;
     color: $backgroundColor;
-    box-shadow: $defaultBoxShadow;
     transition: background .2s ease-out;
+    font-family: $mainFont;
+    cursor: pointer;
+    font-size: .9375em;
 
-    @media (hover: hover) {
-      &:hover {
-        background: mix($backgroundColor, $duskBlue, 10%);
+    &.ghost {
+      background: $backgroundColor;
+      color: $duskBlue;
+
+      @media (hover: hover) {
+        &:hover {
+          background: mix($backgroundColor, $duskBlue, 90%);
+        }
       }
+    }
+
+    &.flat {
+      box-shadow: $defaultBoxShadow;
+
+      @media (hover: hover) {
+        &:hover {
+          background: mix($backgroundColor, $duskBlue, 10%);
+        }
+      }
+    }
+
+    > svg.feather {
+      height: 1em;
     }
   }
 </style>
