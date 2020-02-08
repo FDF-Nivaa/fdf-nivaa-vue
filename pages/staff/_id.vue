64afbed1fe3352bd5d00149a347d49a3c56997e7
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-02-08 14:00:52 +0100

    Rounded the staff photos

--- pages/staff/_id.vue
+++ pages/staff/_id.vue
@@ -1,7 +1,11 @@
 <template>
   <article>
     <h1>{{person.name}}</h1>
-    <CockpitImage class="staff-photo" :src="person.photo.path" />
+    <CockpitImage
+      class="staff-photo"
+      :src="person.photo.path"
+      :format="CockpitImageFormat.ROUND"
+    />
     <div v-html="person.description"></div>
     <p>Telefon: <a :href="`tel:${person.phone}`">{{person.phone}}</a></p>
     <p>E-mail: <a :href="`mailto:${person.email}`">{{person.email}}</a></p>
@@ -9,12 +13,12 @@
 </template>
 
 <script>
-  import CockpitImage from "../../components/CockpitImage"
+  import CockpitImage, {CockpitImageFormat} from "../../components/CockpitImage"
 
   export default {
     components: { CockpitImage },
     data(params) {
-      return { id: params.id }
+      return { id: params.id, CockpitImageFormat }
     },
     computed: {
       person() {
