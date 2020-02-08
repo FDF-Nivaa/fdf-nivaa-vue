875b424968dc7438e872d1824f714417215a9f58
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-02-08 13:11:54 +0100

    Implemented the PageContent component on the front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,26 @@
 <template>
-  <article>
-    <h1>Velkommen til FDF Nivå</h1>
-    <p>FDF er en masse sjov og ballade: Bål, udfordring, fantasi, løb, leg, fællesskab, ildaktiviter, overskridelse af
-      grænser, weekendture og gode kammerater er præcis hvad FDF handler om!</p>
-    <p>Vi holder til i
-      <a href="https://www.google.dk/maps/dir/''/Lerhytten,+Niv%C3%A5vej+2,+2990+Niv%C3%A5/@55.9356791,12.4392635,12z/">Lerhytten,
-        Nivåvej 2, 2990 Nivå</a> — lige ved stationen. Her har vi masser af natur og plads til hygge og udfordring.</p>
-    <p><small><strong>FDF Nivå</strong> hed tidligere <strong>FDF Karlebo</strong>. Vi skiftede navn i august 2017, da
-      det gamle navn skabte forvirring i forhold til vores fysiske placering.</small></p>
+  <PageContent content-id="front-page">
     <aside>
       <h1>Det skal vi næste gang</h1>
       <NextCalendarEvents />
       <p><FancyButton link-to="/calendar"><CalendarIcon/> Se mere i kalenderen</FancyButton></p>
     </aside>
-  </article>
+  </PageContent>
 </template>
 
 <script>
   import FancyButton from "../components/FancyButton"
   import NextCalendarEvents from "../components/NextCalendarEvents"
+  import PageContent from "../components/PageContent"
   import {CalendarIcon,} from 'vue-feather-icons'
 
   export default {
     name: 'index',
-    components: { CalendarIcon, FancyButton, NextCalendarEvents },
-    mounted() {
-      this.$store.dispatch('clearError')
-    },
+    components: { CalendarIcon, FancyButton, NextCalendarEvents,PageContent },
+    async fetch({store}) {
+      store.dispatch('clearError')
+      await store.dispatch('pages/fetchByCustomId', this.contentId)
+    }
   }
 </script>
 
