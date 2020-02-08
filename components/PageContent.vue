90b1d923933c53a543d2d9d1f2b3304ba32eb3fa
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-02-08 14:01:49 +0100

    Added support for pictures in the page content component

--- components/PageContent.vue
+++ components/PageContent.vue
@@ -2,22 +2,39 @@
   <article class="page-content">
     <template v-if="pageContent">
       <h1>{{pageContent.title}}</h1>
-      <div v-html="pageContent.content" />
+      <div class="page-content-wrapper">
+        <figure
+          v-if="pageContent.picture.path"
+          class="page-content-picture-wrapper landscape"
+        >
+          <CockpitImage
+            :src="pageContent.picture.path"
+            class="page-content-picture"
+          />
+        </figure>
+        <div
+          class="page-content-inner"
+          v-html="pageContent.content"
+        />
+      </div>
     </template>
     <slot />
   </article>
 </template>
 
 <script>
+  import CockpitImage from "./CockpitImage"
+
   export default {
     name: 'PageContent',
+    components: { CockpitImage },
     props: {
       contentId: {
         type: String,
         required: true
       }
     },
-    computed:{
+    computed: {
       pageContent() {
         return this.$store.getters['pages/getByCustomId'](this.contentId)
       }
@@ -29,3 +46,58 @@
     },
   }
 </script>
+
+<style
+  lang="scss"
+  scoped
+>
+  .page-content-wrapper {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  .page-content-inner {
+    flex-basis: 20em;
+    flex-grow: 1;
+  }
+
+  .page-content-picture-wrapper {
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-basis: 25em;
+    margin: 0 0 0 2em;
+  }
+
+  .page-content-picture {
+    height: auto;
+    width: 100%;
+  }
+
+  @media (max-width: 50em) {
+    .page-content-wrapper {
+      flex-direction: column;
+    }
+    .page-content-inner {
+      flex-basis: 0;
+    }
+    .page-content-picture-wrapper {
+      margin: 0 auto .5em;
+      width: 25rem;
+      max-width: 100vw;
+      flex-basis: 0;
+
+    }
+  }
+
+  @media (max-width: 27.5rem) {
+    .page-content-picture-wrapper {
+      width: calc(100vw - 1rem);
+      margin: 0 -1.25rem .5em;
+
+      .page-content-picture {
+        border-radius: 0;
+      }
+    }
+  }
+</style>
