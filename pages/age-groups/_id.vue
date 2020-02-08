c5987942b4a51aa53b1eb31e8a47f89703c2cfac
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-02-08 22:58:19 +0100

    Added staff section to the age group page

--- pages/age-groups/_id.vue
+++ pages/age-groups/_id.vue
@@ -1,14 +1,33 @@
 <template>
-  <article>
+  <article v-if="ageGroup">
     <h1>{{ageGroup.title}}</h1>
     <div
       v-html="ageGroup.description"
     ></div>
+    <div class="staff-wrapper">
+      <div class="staff">
+        <h2>Ansvarlig leder</h2>
+        <StaffCard :staff-id="ageGroup.primaryStaff._id" />
+      </div>
+      <div class="staff">
+        <h2>Andre ledere</h2>
+        <StaffCard
+          v-for="person in ageGroup.staff"
+          :staff-id="person._id"
+          :key="person._id"
+        />
+      </div>
+    </div>
   </article>
+  <LoadingAnimation v-else />
 </template>
 
 <script>
+  import LoadingAnimation from "../../components/LoadingAnimation"
+  import StaffCard from "../../components/StaffCard"
+
   export default {
+    components: { LoadingAnimation, StaffCard },
     data(params) {
       return { id: params.id }
     },
@@ -23,5 +42,37 @@
     },
   }
 </script>
-<style scoped>
+<style
+  lang="scss"
+  scoped
+>
+
+  .staff-wrapper {
+    margin-top: 2em;
+    display: flex;
+  }
+
+  .staff {
+    position: relative;
+    flex: 1 1 15em;
+
+    h2 {
+      margin-top: 0;
+    }
+
+    &:first-child {
+      flex-grow: 0;
+      margin-right: 2.5em;
+
+      &:after {
+        content: "";
+        position: absolute;
+        display: block;
+        height: 4em;
+        bottom: 1em;
+        right: 0;
+        border-right: 1px solid mix($duskBlue, transparent);
+      }
+    }
+  }
 </style>
