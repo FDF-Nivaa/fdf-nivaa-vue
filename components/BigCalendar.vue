0ffb6c4aef217751983f78e81cdceb5a4112538d
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-09-10 20:56:03 +0200

    Refactored the big calendar component to use the calendar store

--- components/BigCalendar.vue
+++ components/BigCalendar.vue
@@ -16,125 +16,87 @@
         v-for="event in sortedEvents"
         :key="event.id"
       >
-        <td>{{event.summary}}
+        <td>{{ event.summary }}
           <CalendarLabel :calendar="event.calendar" />
         </td>
-        <td>{{event.location}}</td>
-        <td>{{formatCalendarDateObjects(event.start, event.end)}}</td>
+        <td>{{ event.location }}</td>
+        <td>{{
+            formatDateSpan(event.parsedDates.startDate, event.parsedDates.endDate, event.parsedDates.displayTimeOfDay)
+          }}
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-  import CalendarLabel from "./CalendarLabel"
-  import LoadingAnimation from "./LoadingAnimation"
+import CalendarLabel from "./CalendarLabel"
+import LoadingAnimation from "./LoadingAnimation"
 
-  import {getEvents} from "../apis/google-calendar"
+import {formatDateSpan} from "../utils/format"
 
-  import {formatDateSpan} from "../utils/format"
-
-  export default {
-    name: 'BigCalendar',
-    components: { CalendarLabel, LoadingAnimation },
-    props: {
-      calendars: {
-        type: Array,
-        required: true,
-        validator(arr) {
-          return !arr.some(val => !val.id)
-        }
-      },
-      startDate: {
-        type: Date,
-        required: false,
-        default: undefined
-      },
-      endDate: {
-        type: Date,
-        required: false,
-        default: undefined
+export default {
+  name: 'BigCalendar',
+  components: { CalendarLabel, LoadingAnimation },
+  props: {
+    calendars: {
+      type: Array,
+      required: true,
+      validator(arr) {
+        return !arr.some(val => !val.id)
       }
     },
-    data() {
-      return { events: [], isLoading: true }
+    startDate: {
+      type: Date,
+      required: false,
+      default: undefined
     },
-    computed: {
-      sortedEvents() {
-        return this.events.sort((a, b) => {
-          return a.jsStartDate.getTime() - b.jsStartDate.getTime()
-        })
-      }
-    },
-    mounted() {
-      this.$store.dispatch('age-groups/fetchAllOnce')
-
-      Promise.allSettled(
-        this.calendars.map(calendar => {
-          return this.addEventsFromCalendar(calendar)
-        }))
-        .then(() => {
-          this.isLoading = false
-        })
-    },
-    methods: {
-      addEventsFromCalendar(calendar) {
-        return getEvents(
-          calendar.id,
-          this.startDate,
-          this.endDate
-        ).then(events => {
-          this.addEvents(
-            events.map(event => {
-              return {
-                ...event,
-                calendar: calendar,
-                jsStartDate: new Date(event.start.date || event.start.dateTime)
-              }
-            })
-          )
-        })
-      },
-      addEvents(events) {
-        this.events = [...this.events, ...events]
-      },
-      formatCalendarDateObjects(dateObject1, dateObject2) {
-        if (dateObject1.date) {
-          return formatDateSpan(
-            new Date(dateObject1.date),
-            new Date(dateObject2.date)
-          )
-        } else if (dateObject1.dateTime) {
-          return formatDateSpan(
-            new Date(dateObject1.dateTime),
-            new Date(dateObject2.dateTime),
-            true
-          )
-        }
-      },
+    endDate: {
+      type: Date,
+      required: false,
+      default: undefined
     }
+  },
+  data() {
+    return { events: [], isLoading: true }
+  },
+  computed: {
+    sortedEvents() {
+      return this.$store.getters['calendars/getAllEvents']()
+    }
+  },
+  mounted() {
+    this.$store.dispatch('age-groups/fetchAllOnce')
+    this.$store.dispatch('calendars/fetchAll', {
+      startDate: this.startDate,
+      endDate: this.endDate
+    }).finally(() => this.isLoading = false)
+  },
+  methods: {
+    formatDateSpan,
   }
+}
 </script>
 
 <style
   lang="scss"
   scoped
 >
-  table {
-    width: 100%;
-    text-align: left;
-    border-collapse: collapse;
-  }
+table {
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
 
-  tbody tr:nth-child(odd) {
-    background: $backgroundColorSlightContrast;
-  }
+tbody tr:nth-child(odd) {
+  background: $backgroundColorSlightContrast;
+}
 
-  th, td {
-    padding: .5em;
-  }
+th, td {
+  padding: .5em;
+}
 
-  .calendar-label {
-    float: right;
-  }
+.calendar-label {
+  float: right;
+}
 </style>
--- store/calendars.js
+++ store/calendars.js
@@ -5,6 +5,26 @@ import {
   googleCalendarDateObjectContainsTime
 } from '~/apis/google-calendar'
 import Vue from 'vue'
+import siteConfig from '../site.config'
+import isSameDay from "date-fns/isSameDay"
+
+const googleCalendars = siteConfig.googleCalendars
+
+export function getCalendarConfig(id) {
+  return googleCalendars.find(calendar => calendar.id === id)
+}
+
+export function sortEvents(events) {
+  return events.sort((a, b) => {
+    return a.parsedDates.startDate.getTime() - b.parsedDates.startDate.getTime()
+  })
+}
+
+export function joinEvents(calendars) {
+  return Object.values(calendars)
+    .map(calendar => calendar.events)
+    .reduce((a, b) => a.concat(b), [])
+}
 
 export function state() {
   return { calendars: {} }
@@ -14,32 +34,27 @@ export const getters = {
   getCalendarById: state => id => {
     return state.calendars[id]
   },
-  getNextEvent: state => id => {
+  getNextEvent: (state, getters) => () => {
+    return getters.getAllEvents()[0]
+  },
+  getNextEvents: (state, getters) => () => {
+    const allEvents = getters.getAllEvents()
+    const firstEventStartDate = allEvents[0].parsedDates.startDate
+    const eventsOnSameDate = allEvents.filter(event => isSameDay(event.parsedDates, firstEventStartDate))
+    return eventsOnSameDate
+  },
+  getAllEvents: state => () => {
+    return sortEvents(joinEvents(state.calendars))
+  },
+  getAllEventsForCalendar: state => id => {
+    return state.calendars[id] || null
+  },
+  getNextEventForCalendar: state => id => {
     return state.calendars[id] && state.calendars[id].events[0] || null
   },
   isCalendarLoading: state => id => {
     return state.calendars[id] && state.calendars[id].isLoading
   }
-}
-
-export const mutations = {
-  setEvents(state, { calendarId, events }) {
-    state.calendars[calendarId].events = events.map(event => {
-      return {
-        ...event,
-        parsedDates: {
-          startDate: googleCalendarDateObjectToDate(event.start),
-          endDate: googleCalendarDateObjectToDate(event.end),
-          displayTimeOfDay: googleCalendarDateObjectContainsTime(event.start)
-        }
-      }
-    })
-    state.calendars[calendarId].isLoading = false
-  },
-  initCalendar(state, { calendarId, startDate, endDate }) {
-    Vue.set(state.calendars, calendarId, { startDate, endDate, events: [], isLoading: true })
-  }
-
 }
 
 export const actions = {
@@ -57,9 +72,9 @@ export const actions = {
       }, { root: true })
     })
   },
-  async fetchAll({ commit, dispatch }) {
+  async fetchAll({ commit, dispatch }, {startDate, endDate, limit}) {
     return Promise.all(calendars.map(calendar => {
-      dispatch('fetchEvents', { calendarId: calendar.id })
+      dispatch('fetchEvents', { calendarId: calendar.id, startDate, endDate, limit })
     })).catch((error) => {
       commit('setError', {
         source: 'Google Calendar',
@@ -68,4 +83,36 @@ export const actions = {
       }, { root: true })
     })
   },
+}
+
+export const mutations = {
+  setEvents(state, { calendarId, events }) {
+    const calendarWrapper = state.calendars[calendarId]
+    calendarWrapper.events = events.map(event => {
+      return {
+        ...event,
+        calendar: calendarWrapper.calendar,
+        parsedDates: {
+          startDate: googleCalendarDateObjectToDate(event.start),
+          endDate: googleCalendarDateObjectToDate(event.end),
+          displayTimeOfDay: googleCalendarDateObjectContainsTime(event.start)
+        }
+      }
+    })
+    state.calendars[calendarId].isLoading = false
+  },
+  initCalendar(state, { calendarId, startDate, endDate }) {
+    Vue.set(
+      state.calendars,
+      calendarId,
+      {
+        calendar: getCalendarConfig(calendarId),
+        events: [],
+        isLoading: true,
+        startDate,
+        endDate,
+      }
+    )
+  }
+
 }
