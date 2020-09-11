be5576cdf65ca3f94ef3a06136d8ff2bfc328f78
Thomas Grinderslev <thomas@fdfnivaa.dk>
2020-09-11 08:53:11 +0200

    Added sorting to the posts store

--- store/posts.js
+++ store/posts.js
@@ -1,3 +1,10 @@
 import {createStore} from './store-factory'
 
-export default createStore('posts', 'opslag', 'opslag')
+const store = createStore('posts', 'opslag', 'opslag')
+
+store.mutations.setList = (state, list) => {
+  state.list = list.sort((a, b) => b._created - a._created)
+}
+
+
+export default store
