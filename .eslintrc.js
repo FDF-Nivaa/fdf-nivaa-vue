a44d9d96468797d5570fc0b175f125404ce1acbf
Thomas Grinderslev <thomas@fdfnivaa.dk>
2021-09-11 17:34:07 +0200

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "prettier/prettier": "error",
+    "no-prototype-builtins": "off",
+    "linebreak-style": ["error", "unix"],
+    "lines-between-class-members": ["error", "always"],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "import", next: "*" },
+      { blankLine: "any", prev: "import", next: "import" },
+      { blankLine: "always", prev: "*", next: "export" },
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+      { blankLine: "always", prev: "block-like", next: "*" },
+      { blankLine: "always", prev: "function", next: "*" },
+    ],
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+};
